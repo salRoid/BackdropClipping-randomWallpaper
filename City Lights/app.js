f6444a79b7f39621521dea4ba5d3d24adcf5d75d
Sajal Gupta <gupta.sajal631@gmail.com>
2021-05-13 10:13:16 +0530

    City Lights

---
+++ City Lights/app.js
@@ -0,0 +1,37 @@
+let heading = document.querySelector('h1');
+
+const fetchCities = async () => {
+    const response = await fetch('config.json');
+    const data = await response.json();
+    return data;
+}
+
+const setCity = async () => {
+    const cities = await fetchCities();
+    let prevIndex = 0;
+    // let index = 0;
+    setInterval(() => {
+        let index = getRandomInt(cities.length);
+        if (index === prevIndex) {
+            if (prevIndex + 1 >= cities.length) {
+                index = 0;
+            } else {
+                index += 1;
+            }
+        }
+        const { city, imgId, height, fontSize} = cities[index];
+        console.info(city, imgId, fontSize);
+        heading.textContent = city;
+        heading.style.backgroundImage = `url(img/${imgId})`;
+        heading.style.backgroundPosition = `50% ${height}%`;
+        heading.style.fontSize = fontSize
+        prevIndex = index;
+    }, 3000);
+
+}
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
+
+setCity();
--- app.js
+++
@@ -1,6 +0,0 @@
-
-var imgNumber = Math.floor(Math.random() * 6) + 1;  // returns number between 1 to 6
-
-let heading = document.querySelector('h1');
-console.log(imgNumber);
-heading.style.backgroundImage = `url(img/${imgNumber}.jpg)`;
